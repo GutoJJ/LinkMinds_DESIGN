6999a386485da8ad489bc3297ea0f4e43557f0d9
GutoJJ <augusto-jung@educar.rs.gov.br>
2023-08-11 11:26:29 -0300

    Nav 1.1

--- script.js
+++ script.js
@@ -6,35 +6,37 @@ let flag = 0;
 let aux = 0;
 
 let funcao_nav = () => {
-    if(flag == 0){
+    if(flag === 0){
         console.log("abrindo");
+        bodyfalso.style.height = "95vh"
         bodyfalso.style.marginRight = "-55%";
         bodyfalso.style.scale = "85%";
         bodyfalso.style.borderRadius = "35px";
-        bodyfalso.style.marginTop = "-10%";
+        bodyfalso.style.transition = "0.5s ease-in-out";
         nav.style.display = "flex";
         nav.style.animation = "fadeIn 0.5s ease-in-out";
         nav.style.opacity = "100%";
-        flag =1;
+        flag = 1;
     }
 }
 
 bodyfalso.onclick = () => {
-    if(flag === 1 && aux == 1){
+    if(flag === 1 && aux === 1){
         flag = 0;
         nav.style.animation = "fadeOut 0.5s ease-in-out";
         setTimeout(function() {
+            bodyfalso.style.transition = "0.5s ease-in-out";
+            bodyfalso.style.height = "";
+            bodyfalso.style.scale = ""; 
             bodyfalso.style.marginRight = "";
-            bodyfalso.style.scale = "";
             bodyfalso.style.borderRadius = "";
-            bodyfalso.style.marginTop = "";
             nav.style.display = "";
             nav.style.opacity = "0%";
             aux=0;
           }, 100);
         
     }
-    else if(flag === 1 && aux == 0){
+    else if(flag === 1 && aux === 0){
         aux=1;
     }
 }
